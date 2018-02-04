a9d7608c3c4df910c00f3aba78200571226f64b2
Christopher M. Miles <twitch@nervestaple.com>
2018-02-04 12:16:51 -0500

    Removed debug info

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,13 @@ fn main() {
     // decide who goes first
     let mut player_next = rand::thread_rng().gen_range(0, 2);
 
-    println!("We sit on opposite sides of the table, with the unloaded between us.");
+    println!("We sit on opposite sides of the table, with the unloaded gun between us.");
 
     loop {
-        println!("Chamber:{} / {}, Loaded: {}", chamber_next, CHAMBERS_TOTAL, chamber_loaded);
+        
         if player_next == 0 {
             println!("I place the gun to my head...");
-            println!("...And pull the trigger...");
+            println!("...And pull the trigger... ");
         } else {
             println!("You place the gun to your head...");
             println!("Press the [RETURN] key to pull the trigger.");
